d38f5db7654795f6ff4e546eb63a296617d58072
Eduardoespl <eduardoesp3317@gmail.com>
2025-08-07 11:14:22 -0600

    Initial page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ function App() {
 
   return (
     <>
-      <h1>Grrizzlies Pick Em</h1>
+      <div className='logo-container'>
+        <img src='/logo.png' className="logo" alt="Grizzlies Logo"/>
+      </div>
+      <h1>Grizzlies Pick'Em</h1>
       <p className="read-the-docs">
         Inicia sesión para acceder a las predicciones
       </p>
+      <div className="login-container">
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <button className="login-button">Iniciar sesión</button>
+        </a>
+      </div>
     </>
   )
 }
